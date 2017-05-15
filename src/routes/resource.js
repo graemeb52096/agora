3b21fb8906032b0515095c3fee58279b31f01abc
graemeb52096 <batesg1996@gmail.com>
2017-05-15 17:17:41 -0400

    Added resource route that sends file, not sure if it works

---
+++ src/routes/resource.js
@@ -0,0 +1,24 @@
+var Resource = require('../db/resource');
+
+module.exports = function(router){
+	router.route('/resource/:kind/:id')
+	.get(function(req, res){
+		Resource.findById(req.params.id, function(err, resource){
+			if (err){
+				res.json(err);
+			} else {
+				if (req.kind == 'img'){
+					var filePath = '../uploads/iamges/' + req.params.id;
+				} else if(req.kind == 'aud'){
+					var filePath = '../uploads/audio/' + req.params.id;
+				} else if(req.kind == 'vid'){
+					var filePath = '../uploads/video/' + req.params.id;
+				} else {
+					res.sendStatus(400);
+					return;
+				};
+				res.sendFile(filePath);
+			};
+		});
+	});
+};
